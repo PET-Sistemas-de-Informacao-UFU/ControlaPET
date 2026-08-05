b9f55e1f23c0c0ce1e5090bcf7f842e778d09366
Pedro Furlan <pedrogfurlan1@gmail.com>
2026-08-05 15:58:21 -0300

    chore: adiciona configuração base do Spring Security e JWT

---
+++ src/main/java/br/ufu/facom/petsi/controlaPET/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package br.ufu.facom.petsi.controlaPET.security;
+
+import br.ufu.facom.petsi.controlaPET.model.User;
+import br.ufu.facom.petsi.controlaPET.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
