200976b8b29c0467fdf93ad8cd5147175e3daadd
Pedro Furlan <pedrogfurlan1@gmail.com>
2026-08-04 22:10:26 -0300

    chore: setup inicial do spring boot com postgres, jwt e configs base

---
+++ src/main/java/br/ufu/facom/petsi/controlaPET/ControlaPetApplication.java
@@ -0,0 +1,13 @@
+package br.ufu.facom.petsi.controlaPET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlaPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlaPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufu/facom/petsi/controlaPET/ControlaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufu.facom.petsi.controlaPET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
