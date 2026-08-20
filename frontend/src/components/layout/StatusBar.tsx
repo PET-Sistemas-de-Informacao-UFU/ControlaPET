e94a4f796fd8f8b93325811ec147e8d541c2d45d
Pedro Furlan <pedrogfurlan1@gmail.com>
2026-08-20 10:05:36 -0300

    wip: versao desktop e mobile

--- frontend/src/components/layout/StatusBar.tsx
+++
@@ -1,8 +0,0 @@
-export default function StatusBar() {
-    return (
-        <div className="statusbar">
-            <span>12:30</span>
-            <span>&#x2026; &#x1F6DC; &#x1F50B; </span>
-        </div>
-    );
-}
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import AppLayout from "../components/layout/AppLayout";
+import HomeRoute from "./HomeRoute";
 import Scanner from "../pages/Scanner/Index";
 import Catalogo from "../pages/Catalogo/Index";
 import Auditoria from "../pages/Auditoria/Index";
@@ -7,7 +8,8 @@ import Auditoria from "../pages/Auditoria/Index";
 export const routes = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<AppLayout />}>
-            <Route index path="/" element={<Scanner />} />
+            <Route index path="/" element={<HomeRoute />} />
+            <Route path="/ler" element={<Scanner />} />
             <Route path="/catalogo" element={<Catalogo />} />
             <Route path="/auditoria" element={<Auditoria />} />
         </Route>
