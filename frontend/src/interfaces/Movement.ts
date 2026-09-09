0b3cfda1ccd9e76b94c4ae634b7102b2b432ce9d
Pedro Furlan <pedrogfurlan1@gmail.com>
2026-09-09 12:38:24 -0300

    feat: adiciona interface dos objetos

---
+++ frontend/src/interfaces/Movement.ts
@@ -0,0 +1,24 @@
+export type MovementType = 'INBOUND' | 'OUTBOUND_CONSUMPTION' | 'ADJUSTMENT'
+
+export interface Movement {
+    id: number,
+    userName: string,
+    itemId: number,
+    itemName: string,
+    type: MovementType,
+    notes: string,
+    quantity: number,
+    movementDate: string
+}
+
+export interface CreateMovementRequest {
+    itemId: number,
+    quantity: number,
+    notes: String,
+    type: MovementType
+}
+
+export interface ConsumeItemDTO {
+    itemId: number,
+    quantity: number
+}
